0e3e5afb16351eb9f53c94c5d030cd735599ced3
payn4478 <payn4478@tamu.edu>
2019-01-30 13:24:21 -0600

    Adjusted colors / Added in logs for fire and wind particle effects
    Adjusted colors / Added in logs for fire and wind particle effects

---
+++ Assets/Scripts/LightEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightEffect : MonoBehaviour {
+
+    [SerializeField] Light Firepit;
+    [SerializeField] float MinIntensity;
+    [SerializeField] float MaxIntensity;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
